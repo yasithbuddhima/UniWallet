a25e7c51e90a4cccf0794160719d57c6e9cdb391
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-14 21:16:02 +0530

    Add Auth Page initial files

---
+++ frontend/src/Pages/Auth/AuthPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Authpage.modules.css";
+
+const Authpage = () => {
+  return (
+    <>
+      <div>{/* //TODO: Implement the Login and SignIn Pages in here */}</div>
+    </>
+  );
+};
+
+export default Authpage;
