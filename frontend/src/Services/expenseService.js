ba4ee1f0107b43b3f3dca523017bb262258b72e8
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-17 16:41:23 +0530

    feat: Implement Expense services

---
+++ frontend/src/Services/expenseService.js
@@ -0,0 +1,39 @@
+const API_BASE = process.env.REACT_APP_BACKEND_API_BASE;
+
+// Method to get all Expenses
+const getAllExpenses = async (token) => {
+  const res = await fetch(`${API_BASE}/api/expense`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
+};
+
+const addExpense = async (token, expense) => {
+  const res = await fetch(`${API_BASE}/api/expense/add`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(expense),
+  });
+
+  return res.json();
+};
+
+const deleteExpense = async (token, expense) => {
+  const res = await fetch(`${API_BASE}/api/expense/delete`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(expense),
+  });
+
+  return res.json();
+};
+
+export { getAllExpenses, addExpense, deleteExpense };
