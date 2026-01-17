f3c39842915ca6e71ef2294ab42e6be36ac718de
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-17 15:04:42 +0530

    feat: Implement methods to get all expenses and add expense by uid

---
+++ backend/src/services/expense.service.js
@@ -0,0 +1,35 @@
+import { db } from "../config/firebaseAdmin";
+
+const getExpenses = async (uid) => {
+  const snapshot = await db
+    .collection("users")
+    .doc(uid)
+    .collection("expenses")
+    .orderBy("date", "desc")
+    .get();
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+const addExpense = async (uid, expense) => {
+  const ref = await db
+    .collection("users")
+    .doc(uid)
+    .collection("expenses")
+    .add({
+      ...expense,
+      createdAt: new Date().toISOString,
+    });
+
+  return {
+    id: ref.id,
+    ...expense,
+  };
+};
+
+// TODO: Add method to delete expense and update expense
+
+module.exports = { getExpenses, addExpense };
