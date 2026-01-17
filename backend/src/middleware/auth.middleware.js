6cf67d8f1d75da6ad37391bf612b4017a764ecdf
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-17 20:42:26 +0530

    feat: Implement auth middleware

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const auth = require("../config/firebaseAdmin");
+
+async function authMiddleware(req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header || !header.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const token = header.split(" ")[1];
+    const decoded = await auth.verifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error("Auth Error:", error);
+    res.status(401).json({ message: "Invalid Token" });
+  }
+}
+
+module.exports = authMiddleware;
