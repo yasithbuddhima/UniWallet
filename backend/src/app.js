372bf22d7b27e91df9d8146f5a7cb1609b7a0b9d
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-17 20:42:26 +0530

    feat: Implement auth middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,16 +3,24 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
 const expenseRoutes = require("./routes/expense.routes");
+const authMiddleware = require("./middleware/auth.middleware");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/exspense", expenseRoutes);
+app.use("/api/exspense", authMiddleware, expenseRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend is working on port 5000" });
+});
+
+app.get("/api", authMiddleware, (req, res) => {
+  res.json({
+    message: "User is authenticated and Backend is working on port 5000",
+  });
 });
 
 module.exports = app;
