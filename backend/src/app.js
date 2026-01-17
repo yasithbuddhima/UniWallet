cf8c81b255b2b9f4c4c02dfe857c15dc0d969dd9
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-17 10:11:26 +0530

    feat: Implement auth routes in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.routes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend is working on port 5000" });
