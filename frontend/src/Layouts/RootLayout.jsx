e0293d21ccb871fd9ab125a448de77b10ca68be3
Yasith Buddhima <yasithbuddhima@gmail.com>
2026-01-22 23:18:54 +0530

    feat: Implement Reminder provider

--- frontend/src/Layouts/RootLayout.jsx
+++ frontend/src/Layouts/RootLayout.jsx
@@ -1,18 +1,21 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { ReminderProvider } from "../context/ReminderContext";
 
 export default function RootLayout() {
   return (
     <>
-      <nav>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-        <NavLink to="/expenses">Expenses</NavLink>
-        <NavLink to="/reminders">Reminders</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-      </nav>
+      <ReminderProvider>
+        <nav>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/expenses">Expenses</NavLink>
+          <NavLink to="/reminders">Reminders</NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+        </nav>
 
-      <main>
-        <Outlet />
-      </main>
+        <main>
+          <Outlet />
+        </main>
+      </ReminderProvider>
     </>
   );
 }
